0f53815cac3bec0132b7d5a5bb70978d221e0c79
François Michaud <fmichaud@deepnox.io>
2021-05-27 13:58:29 +0200

    Post schema

--- data/app/src/models/post.model.js
+++ data/app/src/models/post.model.js
@@ -1,3 +1,21 @@
 const mongoose = require('mongoose')
 
+/**
+ * Correspond peu ou prou à la structure d'une table de base de données.
+ *
+ * ie. CREATE TABLE (id, field...) en SQL.
+ *
+ * @type {module:mongoose.Schema<Document, Model<any, any, any>, undefined>}
+ */
+const PostSchema = new mongoose.Schema({
+    nickname: {type: String, maxlength: 32, validate:/[^\s]+/},
+    text: {type: String, maxlength: 140},
+    isActive: {type: Boolean, default: True},
+}, {versionKey: false})
+
+/**
+ * Assigner le schéma à la connexion en cours vers Mongoose.
+ *
+ */
+const Post = mongoose.Model('Post', PostSchema)
 
