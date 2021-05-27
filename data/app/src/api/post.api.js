4944ea9ac723e9519843354eaa5596a9df53d937
François Michaud <fmichaud@deepnox.io>
2021-05-27 15:02:56 +0200

    Fix bug

--- data/app/src/api/post.api.js
+++ data/app/src/api/post.api.js
@@ -5,15 +5,28 @@ const LOG = bunyan.createLogger({name: 'PostApi'})
 const {PostServices} = require("../services");
 const postApi = require('express').Router()
 
+
+
+const validateRegister = (req, res, next) => {
+    // username min length 3
+    if (!req.body.username || req.body.username.length < 3) {
+        return res.status(400).send({
+            msg: 'Please enter a username with min. 3 chars'
+        });
+    }
+    next();
+}
+
 postApi.post('/', async (req, res, next) => {
     const {nickname, text, isActive} = req.body
     LOG.debug(`post(nickname=${nickname}, text=${text}, isActive=${isActive}`)
-    try {
+    // try {
         let r = await PostServices.create(nickname, text, isActive)
         res.json(r)
-    } catch (err) {
-        res.status(400).json({message: `Unexcepted error ${err.msg}`})
-    }
+    // } catch (err) {
+        //     res.status(400).json({message: `Unexcepted error ${err.msg}`})
+        // }
 })
+
 
 module.exports = postApi
--- data/app/src/index.js
+++ data/app/src/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const bunyan = require('bunyan')
+
+const LOG = bunyan.createLogger({name: 'index', level: 'DEBUG'})
 
 const {PostApi} = require('./api')
 
@@ -13,6 +16,13 @@ const main = () => {
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use('/post', PostApi)
+    app.use((err, req, res, next) => {
+        const simplified_err = {message: err.message, stack: err.stack}
+        LOG.error(simplified_err)
+        res.status(500).send(simplified_err)
+    })
+
+
     const port = 3000 || process.env.PORT
     app.listen(port, () => {
         console.log(`Microblogging API server is listening on port: ${port}`)
